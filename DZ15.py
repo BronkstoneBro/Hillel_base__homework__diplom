51cc96dd6fd588e2496b9b8d821a14f021c6ca85
Bronislav <atlantwindow@gmail.com>
2023-09-24 15:14:38 +0300

    Finish dz

--- DZ15.py
+++ DZ15.py
@@ -1,7 +1,15 @@
 def finish(input_str):
     if input_str.lower() in ["вихід", "exit", "quit", "e", "q"]:
         return "exit"
-    elif input_str.lstrip('-').isdigit():
+
+    if input_str.startswith('--'):
+        return f"Ви ввели неправильне число: {input_str}"
+
+    if input_str.startswith('-'):
+        number = int(input_str)
+        return f"Ви ввели від'ємне ціле число: {number}"
+
+    elif input_str.isdigit():
         number = int(input_str)
         if number == 0:
             return "Ви ввели нуль"
@@ -12,7 +20,9 @@ def finish(input_str):
     else:
         try:
             number = float(input_str.replace(",", "."))
-            if number > 0:
+            if number == 0:
+                return "Ви ввели нуль"
+            elif number > 0:
                 return f"Ви ввели позитивне дробове число: {number}"
             else:
                 return f"Ви ввели від'ємне дробове число: {number}"
@@ -22,7 +32,7 @@ def finish(input_str):
 
 while True:
     user_input = input("Введіть будь яке число, або 'вихід', "
-                       "'exit', 'quit', 'e', 'q'  для виходу: ")
+                       "'exit', 'quit', 'e', 'q' для виходу: ")
     result = finish(user_input)
     if result == "exit":
         break
