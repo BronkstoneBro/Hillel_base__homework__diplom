26af39675cf119216b1eccdbbe97b8e3eb084c3c
Bronislav <atlantwindow@gmail.com>
2023-09-25 13:42:42 +0300

    Finish dz

--- CSV.py
+++ CSV.py
@@ -2,10 +2,10 @@ import json
 import csv
 import random
 
-with open ("dictionary.json", "r") as json_file:
+with open("dictionary.json", "r") as json_file:
     data = json.load(json_file)
 
-with open ('CSV_read.csv', 'w', newline="") as csv_file:
+with open('CSV_read.csv', 'w', newline="") as csv_file:
     option_open = ["ID", "Имя", "Возраст", "Телефон"]
     writer = csv.DictWriter(csv_file, fieldnames=option_open)
 
@@ -19,9 +19,11 @@ with open ('CSV_read.csv', 'w', newline="") as csv_file:
         if have_phone:
             numbers = ['095', '066', '098', '096', '050', '097']
             number = random.choice(numbers)
-            phone_numbers = "".join(random.choice("0123456789") for _ in range(7))
+            phone_numbers = "".join(random.choice("0123456789")
+                                    for _ in range(7))
             phone = f'{number}{phone_numbers}'
 
-        writer.writerow({"ID": id, "Имя": name, "Возраст": age, "Телефон": phone})
+        writer.writerow({"ID": id, "Имя": name,
+                         "Возраст": age, "Телефон": phone})
 
-print ("save in CSV-file 'CSV_read.csv'")
+print("save in CSV-file 'CSV_read.csv'")
